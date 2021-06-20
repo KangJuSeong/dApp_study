2f3056a607cd35573aefd831761568d0cf455683
강주성 <kjs@kookmin.ac.kr>
2021-06-20 15:03:56 +0000

    write CoinToFlip.sol

--- react-dapp/contracts/CoinToFlip.sol
+++ react-dapp/contracts/CoinToFlip.sol
@@ -4,3 +4,22 @@
 // 3. 동전 던지기 실행(난수 사용)
 // 4. 베팅한 동전이 나오면 유저 주소로 수수료를 제외한 이더 송금
 // 5. 장부에 베팅 정보 리셋
+
+pragma solidity ^0.4.24;
+
+contract CoinToFlip {
+	uint constant MAX_CASE = 2;
+	uint constant MIN_BET = 0.01 ether;
+	uint constant MAX_BET = 10 ether;
+	uint constant HOUSE_FEE_PERCENT = 5;
+	uint constant HOUSE_MIN_FEE = 0.005 ether;
+	
+	adress public owner;
+	uint public lockedInBets;
+	
+	struct Bet {
+		uint amount;
+		uint8 numOfBetBit;
+		uint placeBlockNumber;
+	}
+}
